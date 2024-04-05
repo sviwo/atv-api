39371e765a1683a26b1fede0f392fc3db9a27d91
Gavine <gavinegaowen@gmail.com>
2024-04-05 15:09:23 +0800

    fix

--- internal/logic/home/home.go
+++ internal/logic/home/home.go
@@ -20,7 +20,13 @@ type sHome struct{}
 func (s sHome) GetHomeData(ctx context.Context) (out *model.HomeDataOutput) {
 	//todo 等待调用硬件电量和公里数
 	out = new(model.HomeDataOutput)
+	//根据物模型获取所有属性数据 根据情况选择
 	res, _ := service.DevDevice().GetLatestProperty(ctx, "sviwo_atv")
+	//根据物模型获取单个属性数据 根据情况选择
+	//res, _ := service.DevDevice().GetProperty(ctx, &model.DeviceGetPropertyInput{
+	//	DeviceKey:   "sviwo_atv",
+	//	PropertyKey: "Vehspeed",
+	//})
 	glog.Print(ctx, res)
 	out.Version = service.Version().GetNewVersion(ctx)
 	return
