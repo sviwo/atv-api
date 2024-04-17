c6fda688cf9df9f65a75973ff552622238a72f8d
Gavine <gavinegaowen@gmail.com>
2024-04-17 22:04:59 +0800

    修改个人中心编辑个人信息空值问题

--- internal/logic/user/user.go
+++ internal/logic/user/user.go
@@ -180,7 +180,7 @@ func (s *sUser) Info(ctx context.Context) (out *model.UserInfoOutput) {
 EditInfo 编辑用户资料
 */
 func (s *sUser) EditInfo(ctx context.Context, in model.EditInfoInput) {
-	if _, err := dao.User.Ctx(ctx).OmitNilData().Update(
+	if _, err := dao.User.Ctx(ctx).OmitEmptyData().Update(
 		in, dao.User.Columns().UserId, service.BizCtx().Get(ctx).Data.Get(consts.ContextKeyUserId),
 	); err != nil {
 		panic(err)
