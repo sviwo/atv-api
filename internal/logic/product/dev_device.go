48724dd445ce8735b4d6933b807860c66a61cae4
gyc <821317143@qq.com>
2024-07-29 10:32:02 +0800

    fix

--- internal/logic/product/dev_device.go
+++ internal/logic/product/dev_device.go
@@ -270,7 +270,7 @@ func (s *sDevDevice) GetDeviceSecret(ctx context.Context, deviceName string) (
 	if err = gconv.Struct(data, &out); err != nil {
 		panic(err)
 	}
-	out.MqttHostUrl = g.Cfg().MustGet(ctx, "aliyun.iot.amqp.host").String()
+	out.MqttHostUrl = g.Cfg().MustGet(ctx, "aliyun.iot.mqtt.host").String()
 	return
 }
 
